7b13e87a5b4c82dfc333ddfc76b56eb2b47cf3ea
Arc <33088785+arcbtc@users.noreply.github.com>
2023-02-20 00:27:27 +0000

    test satspay check

--- __init__.py
+++ __init__.py
@@ -1,25 +1,36 @@
+from http import HTTPStatus
+
+from starlette.exceptions import HTTPException
+
 from fastapi import APIRouter
 from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
 
-db = Database("ext_tipjar")
+from os.path import exists
 
-tipjar_ext: APIRouter = APIRouter(prefix="/tipjar", tags=["tipjar"])
+if exists(..satspay.models.py):
+    db = Database("ext_tipjar")
 
-tipjar_static_files = [
-    {
-        "path": "/tipjar/static",
-        "app": StaticFiles(directory="lnbits/extensions/tipjar/static"),
-        "name": "tipjar_static",
-    }
-]
+    tipjar_ext: APIRouter = APIRouter(prefix="/tipjar", tags=["tipjar"])
+
+    tipjar_static_files = [
+        {
+            "path": "/tipjar/static",
+            "app": StaticFiles(directory="lnbits/extensions/tipjar/static"),
+            "name": "tipjar_static",
+        }
+    ]
 
 
-def tipjar_renderer():
-    return template_renderer(["lnbits/extensions/tipjar/templates"])
+    def tipjar_renderer():
+        return template_renderer(["lnbits/extensions/tipjar/templates"])
 
+    from .views import *  # noqa: F401,F403
+    from .views_api import *  # noqa: F401,F403
+else:
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail="Tipjar does not exist."
+    )
 
-from .views import *  # noqa: F401,F403
-from .views_api import *  # noqa: F401,F403
