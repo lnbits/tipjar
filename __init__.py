1a883926f61232cd378366a29fb0ec732fb6fb0e
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-09-17 11:07:59 +0100

    allow custom path

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -11,14 +10,13 @@ tipjar_ext: APIRouter = APIRouter(prefix="/tipjar", tags=["tipjar"])
 tipjar_static_files = [
     {
         "path": "/tipjar/static",
-        "app": StaticFiles(directory="lnbits/extensions/tipjar/static"),
         "name": "tipjar_static",
     }
 ]
 
 
 def tipjar_renderer():
-    return template_renderer(["lnbits/extensions/tipjar/templates"])
+    return template_renderer(["tipjar/templates"])
 
 
 from .views import *  # noqa: F401,F403
