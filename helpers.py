afb5b4308a834136fb44a71d74aad9a6cf48ce61
dni ⚡ <office@dnilabs.com>
2025-02-26 13:55:16 +0100

    fix: tipjar warning if satspay not installed
    closes #19
    - cannot create tipjar if satspay is not installed
    - frontend throws warning satspay not installed

--- helpers.py
+++ helpers.py
@@ -2,6 +2,22 @@ import httpx
 from lnbits.settings import settings
 
 
+async def check_satspay(api_key: str) -> bool:
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {"X-API-KEY": api_key}
+            r = await client.get(
+                url=f"http://{settings.host}:{settings.port}/satspay/api/v1",
+                headers=headers,
+            )
+            r.raise_for_status()
+            return True
+        except Exception as exc:
+            print("Failed to check satspay")
+            print(exc)
+            return False
+
+
 async def create_charge(data: dict, api_key: str) -> str:
     async with httpx.AsyncClient() as client:
         headers = {"X-API-KEY": api_key}
